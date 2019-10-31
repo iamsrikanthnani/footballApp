b6f7a72092339a2573753219bc21834d4d2a0461
Ahmed Raouf <edbraouf@gmail.com>
2019-10-30 20:08:05 -0500

    navigation folder moved

---
+++ src/screens/profile/SecProfile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class SecProfile extends Component {
+  render() {
+    return (
+      <View>
+        <Text> SecProfile </Text>
+      </View>
+    )
+  }
+}
+
+export default SecProfile
---
+++ src/screens/profile/ThirdProfile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class ThirdProfile extends Component {
+  render() {
+    return (
+      <View>
+        <Text> ThirdProfile </Text>
+      </View>
+    )
+  }
+}
+
+export default ThirdProfile
